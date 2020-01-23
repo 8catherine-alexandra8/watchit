0289098c72182d8b04ca1607f45781a7080b9e87
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-01-23 13:32:39 -0700

    added start function that doesn't really do anything yet, but is wrapped in debounce so that it won't be invoked till after 100ms have passed without an add event being triggered

--- index.js
+++ index.js
@@ -6,7 +6,17 @@
 const debounce = require('lodash.debounce');
 //require in the chokidar package
 const chokidar = require('chokidar');
-
+//declare start function that will eventually be used to start
+//up a user's code. It's not doing anything yet except console
+//logging a phrase.  The function is wrapped in debounce to keep it
+//from being called too many times in quick succession
+const start = debounce(() => {
+	console.log('STARTING USERS PROGERAM');
+	//added 100ms as second argument to debounce to tell debounce
+	//that I want 100ms to pass, without the add event being
+	//triggered before the start function should actually be
+	//invoked
+}, 100);
 //using the syntax outlined in chokidar documentation,
 //tell chokidar to watch the current directory and add
 //an event listener for three different events that will
