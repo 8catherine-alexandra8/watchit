07b0f66fba59cc6c312020632f9707288303cdb7
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-01-23 14:58:25 -0700

    added code to run caparol to create a help display for users

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const chokidar = require('chokidar');
 //itself. So program will make a lot more sense to anyone
 //reading the code, deciphering what it's doing.
 const program = require('caporal');
+//following the syntax outlined in the caporal documentation,
+//call prgram and chain on the parts needed
+program
+	//chain on version with whatever version we decide this is
+	.version('0.0.1')
+	//chain on argument and specify the argument that I expect
+	//this program to be invoked with. It will be optional so using
+	//square brackets.  It's optional because we will set it up
+	//so that index.js will run by default if no filename is provided
+	//the second argument for argument is documentation
+	//for the user. Put in a string that you want the user to see
+	//if they ever try to print out some help about what this command
+	//is doing
+	.argument('[filename]', 'Name of a file to execute')
+	//there aren't any more arguments or options that we need so we
+	//can move on to chaining on the action call to invoke the program
+	//the first argument that action will receive is args, which will
+	//be an object of all the different arguments that were provided
+	.action((args) => {
+		console.log(args);
+	});
+//to get the above program to work, we need to add the following
+//line, per caporal documentation
+program.parse(process.argv);
 //declare start function that will eventually be used to start
 //up a user's code. It's not doing anything yet except console
 //logging a phrase.  The function is wrapped in debounce to keep it
