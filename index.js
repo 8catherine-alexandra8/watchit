3d5c6ab6c9c97ab353d4718ea1e6176b5358d84a
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-01-23 15:01:26 -0700

    cut all existing code for debounce and chokdir and pasted it in as the function for action

--- index.js
+++ index.js
@@ -30,35 +30,34 @@ program
 	//the first argument that action will receive is args, which will
 	//be an object of all the different arguments that were provided
 	.action((args) => {
-		console.log(args);
+		//declare start function that will eventually be used to start
+		//up a user's code. It's not doing anything yet except console
+		//logging a phrase.  The function is wrapped in debounce to keep it
+		//from being called too many times in quick succession
+		const start = debounce(() => {
+			console.log('STARTING USERS PROGERAM');
+			//added 100ms as second argument to debounce to tell debounce
+			//that I want 100ms to pass, without the add event being
+			//triggered before the start function should actually be
+			//invoked
+		}, 100);
+		//using the syntax outlined in chokidar documentation,
+		//tell chokidar to watch the current directory and add
+		//an event listener for three different events that will
+		//result in corresponding console logged responses
+		chokidar
+			.watch('.')
+			//this function will occur both when a file is added, but
+			//also the first time our program is opened each time
+			//and chokidar sees all the files that are in it
+			//.on('add', () => console.log('FILE ADDED'))
+			//replaced callback from above function with the start
+			//function passed in as the second argument
+			.on('add', start)
+			.on('change', () => console.log('FILE CHANGED'))
+			//this is called when a file is deleted
+			.on('unlink', () => console.log('FILE UNLINKED'));
 	});
 //to get the above program to work, we need to add the following
 //line, per caporal documentation
 program.parse(process.argv);
-//declare start function that will eventually be used to start
-//up a user's code. It's not doing anything yet except console
-//logging a phrase.  The function is wrapped in debounce to keep it
-//from being called too many times in quick succession
-const start = debounce(() => {
-	console.log('STARTING USERS PROGERAM');
-	//added 100ms as second argument to debounce to tell debounce
-	//that I want 100ms to pass, without the add event being
-	//triggered before the start function should actually be
-	//invoked
-}, 100);
-//using the syntax outlined in chokidar documentation,
-//tell chokidar to watch the current directory and add
-//an event listener for three different events that will
-//result in corresponding console logged responses
-chokidar
-	.watch('.')
-	//this function will occur both when a file is added, but
-	//also the first time our program is opened each time
-	//and chokidar sees all the files that are in it
-	//.on('add', () => console.log('FILE ADDED'))
-	//replaced callback from above function with the start
-	//function passed in as the second argument
-	.on('add', start)
-	.on('change', () => console.log('FILE CHANGED'))
-	//this is called when a file is deleted
-	.on('unlink', () => console.log('FILE UNLINKED'));
